895a88d9b25295c16e95de2b7bf0ec2cf56c6008
geokoko <geokoko3@outlook.com.gr>
2025-02-26 05:32:01 +0200

    Minor changes in order to fix building errors

--- src/main/java/com/todo/App.java
+++ src/main/java/com/todo/App.java
@@ -1,11 +1,17 @@
 package com.todo;
 
-public class App {
+import com.todo.views.TaskManagerUI;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) {
-        TaskManagerUI.launch(args);
+        launch(args);
     }
 
-	public String getGreeting() {
-		return "Hello, Task Manager!";
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        new TaskManagerUI().start(primaryStage); // Delegate to TaskManagerUI
+    }
 }
+
